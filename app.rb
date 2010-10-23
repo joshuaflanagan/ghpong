d5a5630303c41640e286ffc9ad7667cc5d5c031c
Joshua Flanagan <joshuaflanagan@gmail.com>
2010-10-22 23:40:31 -0500

    posts a label to issue 4 of gitk-demo

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require 'sinatra'
+require 'net/http'
+
+set :ghuser, ENV['GH_USER']
+set :ghpass, ENV['GH_PASSWORD']
+
 get '/' do
   "GitHub Pong"
+end
+
+get '/ping/:label' do
+  server = "github.com"
+  api_path = "/api/v2/yaml"
+  repo = "joshuaflanagan/gitk-demo"
+  issue = 4
+  add_label = "/issues/label/add/#{repo}/#{params[:label]}/#{issue}"
+  request_path = api_path + add_label
+  response = nil
+  Net::HTTP.start(server) {|http|
+      req = Net::HTTP::Get.new(request_path)
+      req.basic_auth settings.ghuser + "/token", settings.ghpass 
+      response = http.request(req)
+    }
+  "Added Label #{ params[:label]} via #{ request_path } returned #{response.code}"
 end
